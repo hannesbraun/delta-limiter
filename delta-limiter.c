d90e1cf679aef6dbf81d3375e3e6f0472e885914
Hannes Braun <hannesbraun@mail.de>
2021-11-05 18:24:18 +0100

    Progress
    The plugin sounds fine to me now

--- delta-limiter.c
+++ delta-limiter.c
@@ -1,9 +1,8 @@
-#include "lv2/core/lv2.h"
+#include <lv2/core/lv2.h>
 
 #include <math.h>
 #include <stdint.h>
 #include <stdlib.h>
-#include <string.h>
 
 #define DL_URI "https://github.com/hannesbraun/delta-limiter"
 
@@ -13,9 +12,9 @@ typedef enum {
 	OUTPUT_L = 2,
 	OUTPUT_R = 3,
 	GAIN_IN = 4,
-	SHAPE = 5,
-	LIMIT = 6,
-	GAIN_OUT = 7
+	LIMIT = 5,
+	GAIN_OUT = 6,
+	DRY_WET = 7
 } PortIndex;
 
 typedef struct {
@@ -23,14 +22,16 @@ typedef struct {
 	const float* gain_in;
 	const float* limit;
 	const float* gain_out;
+	const float* dry_wet;
 	const float* input_l;
 	const float* input_r;
 	float* output_l;
 	float* output_r;
 
+	// Last output value (before amplification)
 	float last_out_l;
 	float last_out_r;
-} DeltaLimiter;
+} Delta_Limiter;
 
 static LV2_Handle instantiate(
 	const LV2_Descriptor* descriptor,
@@ -38,26 +39,14 @@ static LV2_Handle instantiate(
 	const char* bundle_path,
 	const LV2_Feature* const* features)
 {
-	DeltaLimiter* dl = (DeltaLimiter*) calloc(1, sizeof(DeltaLimiter));
+	Delta_Limiter* dl = (Delta_Limiter*) calloc(1, sizeof(Delta_Limiter));
 	return (LV2_Handle) dl;
 }
 
 static void connect_port(LV2_Handle instance, uint32_t port, void* data) {
-	DeltaLimiter* dl = (DeltaLimiter*) instance;
+	Delta_Limiter* dl = (Delta_Limiter*) instance;
 
 	switch ((PortIndex) port) {
-	case GAIN_IN:
-		dl->gain_in = (const float*) data;
-		break;
-	case SHAPE:
-		dl->shape = (const float*) data;
-		break;
-	case LIMIT:
-		dl->limit = (const float*) data;
-		break;
-	case GAIN_OUT:
-		dl->gain_out = (const float*) data;
-		break;
 	case INPUT_L:
 		dl->input_l = (const float*) data;
 		break;
@@ -70,71 +59,83 @@ static void connect_port(LV2_Handle instance, uint32_t port, void* data) {
 	case OUTPUT_R:
 		dl->output_r = (float*) data;
 		break;
+	case GAIN_IN:
+		dl->gain_in = (const float*) data;
+		break;
+	case LIMIT:
+		dl->limit = (const float*) data;
+		break;
+	case GAIN_OUT:
+		dl->gain_out = (const float*) data;
+		break;
+	case DRY_WET:
+		dl->dry_wet = (const float*) data;
 	}
 }
 
 static void activate(LV2_Handle instance) {
-	DeltaLimiter* dl = (DeltaLimiter*) instance;
+	Delta_Limiter* dl = (Delta_Limiter*) instance;
 	dl->last_out_l = 0.0;
 	dl->last_out_r = 0.0;
 }
 
-// #define DB_CO(g) ((g) > -90.0f ? powf(10.0f, (g)*0.05f) : 0.0f)
-
 static void run(LV2_Handle instance, uint32_t n_samples) {
-	DeltaLimiter* dl = (DeltaLimiter*) instance;
+	Delta_Limiter* dl = (Delta_Limiter*) instance;
 
-	const float gain_in = *(dl->gain_in);
-	const float shape = *(dl->shape);
-	const float limit = *(dl->limit);
-	const float gain_out = *(dl->gain_out);
 	const float* const input_l = dl->input_l;
 	const float* const input_r = dl->input_r;
 	float* const output_l = dl->output_l;
 	float* const output_r = dl->output_r;
+	const float gain_in = *(dl->gain_in);
+	const float limit = 0.2 * powf(*(dl->limit), 2.0);
+	const float gain_out = *(dl->gain_out);
+	const float dry_wet = *(dl->dry_wet);
 	float last_out_l = dl->last_out_l;
 	float last_out_r = dl->last_out_r;
 
-	// const float coef = DB_CO(gain);
-
-	if (limit < 0.00003) {
-		// Prevent DC
-		memset(output_l, 0.0, n_samples * sizeof(float));
-		memset(output_r, 0.0, n_samples * sizeof(float));
-		dl->last_out_l = 0.0;
-		dl->last_out_r = 0.0;
-		return;
-	}
-
-	// tanh(xy)/tanh(y)
-
 	float l, r;
 	float delta;
+	float fade_out = (limit < 0.0001f) ? limit / 0.0001 : 1.0f;
 	for (uint32_t pos = 0; pos < n_samples; pos++) {
+		// Apply input gain
 		l = input_l[pos] * gain_in;
 		r = input_r[pos] * gain_in;
 
+		// Manual loop unrolling instead of using an array for left/right
+
+		// Determine delta
 		delta = l - last_out_l;
+
+		// Clip delta
 		if (delta > limit) {
 			delta = limit;
 		} else if (delta < -limit) {
 			delta = -limit;
 		}
+
+		// Calculate resulting signal for left channel
 		l = last_out_l + delta;
 
+		// Determine delta
 		delta = r - last_out_r;
+
+		// Clip delta
 		if (delta > limit) {
 			delta = limit;
 		} else if (delta < -limit) {
 			delta = -limit;
 		}
+
+		// Calculate resulting signal for right channel
 		r = last_out_r + delta;
 
+		// Remember last output value
 		last_out_l = l;
 		last_out_r = r;
 
-		output_l[pos] = l * gain_out;
-		output_r[pos] = r * gain_out;
+		// Apply output gain, fadeout and dry/wet
+		output_l[pos] = dry_wet * l * gain_out * fade_out + (1 - dry_wet) * input_l[pos];
+		output_r[pos] = dry_wet * r * gain_out * fade_out + (1 - dry_wet) * input_r[pos];
 	}
 
 	dl->last_out_l = last_out_l;
