ae4eded0a062b19df7972bbaca71b8d306185e05
Hannes Braun <hannesbraun@mail.de>
2021-11-05 20:56:36 +0100

    Add copyright header to source file

--- delta-limiter.c
+++ delta-limiter.c
@@ -1,3 +1,8 @@
+// Copyright Hannes Braun 2021.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE or copy at
+// https://www.boost.org/LICENSE_1_0.txt)
+
 #include <lv2/core/lv2.h>
 
 #include <math.h>
